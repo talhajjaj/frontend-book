1abbdfb2f375c88ec90c4beea43c2cd3b22c6754
talhajjaj <taima.hajaj@gmail.com>
2021-07-13 14:06:11 +0300

    lab12

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -5,6 +5,29 @@ import './BestBooks.css';
 import { withAuth0 } from "@auth0/auth0-react";
 
 class MyFavoriteBooks extends React.Component {
+  constructor(props){
+    super(props)
+    this.state={
+      email: '',
+      bookData: [],
+      showBook: false,
+    }
+  }
+  componentDidMount = async () => {
+    const { user } = this.props.auth0
+    await this.setState({
+      email: `${user.email}`
+
+    })
+    let url = `http://localhost:3001/books?userEmail=${this.state.email}`
+    let responsData = await axios.get(url);
+    await this.setState({
+      showBook: true,
+      bookData: responsData.data,
+    })
+  }
+
+
   render() {
     return(
       <Jumbotron>
